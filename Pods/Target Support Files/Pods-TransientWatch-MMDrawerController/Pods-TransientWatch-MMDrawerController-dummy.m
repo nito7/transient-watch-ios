f19f8e3b6e4b87056602b705dfd4a092dd4b741a
naoto0822 <n.h.in.m.h@gmail.com>
2015-04-11 01:43:41 +0900

    gitignore is now working

--- Pods/Target Support Files/Pods-TransientWatch-MMDrawerController/Pods-TransientWatch-MMDrawerController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TransientWatch_MMDrawerController : NSObject
-@end
-@implementation PodsDummy_Pods_TransientWatch_MMDrawerController
-@end
