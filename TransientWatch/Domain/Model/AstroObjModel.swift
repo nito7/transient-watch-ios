1455a04f44a8ce1785968fd5b7d75a8a9fae7518
naoto0822 <n.h.in.m.h@gmail.com>
2015-04-12 01:54:11 +0900

    AstroObjModel クラス追加

---
+++ TransientWatch/Domain/Model/AstroObjModel.swift
@@ -0,0 +1,13 @@
+//
+//  AstroObjModel.swift
+//  TransientWatch
+//
+//  Created by naoto yamaguchi on 2015/04/12.
+//  Copyright (c) 2015年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class AstroObjModel: NSObject {
+   
+}
