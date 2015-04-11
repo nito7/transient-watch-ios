75cb094ac6f1426bd3fc1b8da51ee3b5373a3f82
naoto0822 <n.h.in.m.h@gmail.com>
2015-04-11 14:07:13 +0900

    sampleでグラフ描写

--- TransientWatch/Presentation/ViewController/HomeViewController.swift
+++ TransientWatch/Presentation/ViewController/HomeViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, PNChartDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let lineChart = PNLineChart(frame: CGRectMake(0, 100, CGRectGetWidth(self.view.frame), 200))
+        lineChart.yLabelFormat = "%1.1"
+        lineChart.backgroundColor = UIColor.clearColor()
+        let xLabels = ["1", "2", "3", "4", "5", "6", "7"]
+        lineChart.xLabels = xLabels
+        lineChart.showCoordinateAxis = true
+        
+        lineChart.yFixedValueMax = 300
+        lineChart.yFixedValueMin = 0.0
+        
+        let yLabels = ["0", "50", "100", "150", "200", "250", "300"]
+//        lineChart.yLabels = yLabels
+        lineChart.yLabelHeight = 20
+        lineChart.yLabelFormat = "%1.0f"
+        
+        let dataArray = [
+            NSNumber(double: 10.0),
+            NSNumber(double: 30.0),
+            NSNumber(double: 10.0),
+            NSNumber(double: 45.0),
+            NSNumber(double: 80.0)
+        ]
+        let chartData = PNLineChartData()
+        chartData.dataTitle = "title"
+        chartData.color = UIColor.redColor()
+        chartData.alpha = 0.3
+        chartData.itemCount = UInt(dataArray.count)
+        chartData.inflexionPointStyle = .Triangle
+        chartData.getData = ({(index: UInt) -> PNLineChartDataItem in
+            var yValue = dataArray[Int(index)]
+            var float = CGFloat(yValue)
+            var item = PNLineChartDataItem(y: float)
+            return item
+        })
+        
+        lineChart.chartData = [chartData]
+        lineChart.strokeChart()
+        lineChart.delegate = self
+        self.view.addSubview(lineChart)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +61,20 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    // MARK: - PNChartDelegate
+    
+    func userClickedOnLinePoint(point: CGPoint, lineIndex: Int) {
+        
+    }
+    
+    func userClickedOnLineKeyPoint(point: CGPoint, lineIndex: Int, pointIndex: Int) {
+        
+    }
+    
+    func userClickedOnBarAtIndex(barIndex: Int) {
+        
+    }
+    
     /*
     // MARK: - Navigation
 
