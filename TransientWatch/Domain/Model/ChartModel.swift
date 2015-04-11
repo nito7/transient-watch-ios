c23242a512b25fe638a4ada86bed3d605f0f5af5
naoto0822 <n.h.in.m.h@gmail.com>
2015-04-12 06:15:25 +0900

    chart model

--- TransientWatch/Domain/Model/ChartModel.swift
+++ TransientWatch/Domain/Model/ChartModel.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class ChartModel: NSObject {
     
+    class func GET(
+        #success: ((NSURLSessionDataTask!, Array<Chart>!) -> Void)!,
+        failure: ((NSURLSessionDataTask!, NSError!) -> Void)!) {
+            
+            var chartArray: [Chart] = []
+            
+            API.sharedInstance.GET(
+                "http://www12480uf.sakura.ne.jp:3000/lightcurves/416",
+                parameters: nil,
+                success: { (task: NSURLSessionDataTask!, result: AnyObject!) -> Void in
+                    var responseArray = result as NSArray
+                    
+                    for response in responseArray {
+                        let chartResponse = response as [String: AnyObject]
+                        let chart = Chart(response: chartResponse)
+                        chartArray.append(chart)
+                    }
+                    
+                    success(task, chartArray)
+                },
+                failure: {(task: NSURLSessionDataTask!, error: NSError!) -> Void in
+                    failure(task, error)
+                }
+            )
+    }
 }
