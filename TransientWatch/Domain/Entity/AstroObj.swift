fc44086a462cf21b8ef89c0a9afd46191ce1863f
naoto0822 <n.h.in.m.h@gmail.com>
2015-04-12 01:53:31 +0900

    astro obj entity

--- TransientWatch/Domain/Entity/AstroObj.swift
+++ TransientWatch/Domain/Entity/AstroObj.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class AstroObj: NSObject {
+    
+    // MARK: - Property
+    
+    var id: String?
+    var name: String?
+    var ra: String?
+    var dec: String?
+    var astroClassId: String?
+    var link: String?
+    
+    // MARK: - LifeCycle
+    
+    init(response: [String: AnyObject?]) {
+        super.init()
         
+        let id = response["id"] as? String
+        let name = response["name"] as? String
+        let ra = response["ra"] as? String
+        let dec = response["dec"] as? String
+        let astroClassId = response["astro_class_id"] as? String
+        let link = response["link"] as? String
+        
+        self.id = id
+        self.name = name
+        self.ra = ra
+        self.dec = dec
+        self.astroClassId = astroClassId
+        self.link = link
+    }
 }
