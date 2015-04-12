849d1bab77e50677a2f8b2655a2493d7d33febeb
naoto0822 <n.h.in.m.h@gmail.com>
2015-04-12 11:13:22 +0900

    chart cell クラス

---
+++ TransientWatch/Presentation/View/ChartCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChartCell.swift
+//  TransientWatch
+//
+//  Created by naoto yamaguchi on 2015/04/12.
+//  Copyright (c) 2015年 naoto yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ChartCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
